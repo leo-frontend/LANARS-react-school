56cfd7a31bd0e0dea1ee63416958d333fc240b93
31f <alex@lanars.com>
2022-08-09 13:44:39 +0300

    Complete storage service

--- src/core/services/Storage.ts
+++ src/core/services/Storage.ts
@@ -1,20 +1,75 @@
-export class Storage {
+import { IDBPDatabase, openDB } from 'idb';
+/* eslint no-console: 0 */  // --> OFF
+class Storage {
 
   db: any;
 
-  constructor() {
-    const DBOpenRequest = window.indexedDB.open('LANARS-react-school', 1);
-
-    DBOpenRequest.onerror = () => {
-      console.error('Error loading database.');
-    };
-
-    DBOpenRequest.onsuccess = () => {
-      console.info('Database initialized.');
-      this.db = DBOpenRequest.result;
-    };
+  constructor(private dbName = 'LANARS-react-school') {
   }
 
-};
+  public async createObjectStore(tableNames: string[]) {
+    try {
+      this.db = await openDB(this.dbName, 1, {
+        upgrade(db: IDBPDatabase) {
+          for (const tableName of tableNames) {
+            if (db.objectStoreNames.contains(tableName)) {
+              continue;
+            }
+            db.createObjectStore(tableName, { autoIncrement: true, keyPath: 'id' });
+          }
+        },
+      });
+    } catch (error) {
+      return false;
+    }
+  }
+
+  // taken from https://javascript.plainenglish.io/working-with-indexeddb-in-typescript-react-ad504a1bdae3
+  async getValue(tableName: string, id: number) {
+    const tx = this.db.transaction(tableName, 'readonly');
+    const store = tx.objectStore(tableName);
+    const result = await store.get(id);
+    console.log('Get Data ', JSON.stringify(result));
+    return result;
+  }
+
+  async getAllValue(tableName: string) {
+    const tx = this.db.transaction(tableName, 'readonly');
+    const store = tx.objectStore(tableName);
+    const result = await store.getAll();
+    console.log('Get All Data', JSON.stringify(result));
+    return result;
+  }
+
+  async putValue(tableName: string, value: Object) {
+    const tx = this.db.transaction(tableName, 'readwrite');
+    const store = tx.objectStore(tableName);
+    const result = await store.put(value);
+    console.log('Put Data ', JSON.stringify(result));
+    return result;
+  }
+
+  async putBulkValue(tableName: string, values: Object[]) {
+    const tx = this.db.transaction(tableName, 'readwrite');
+    const store = tx.objectStore(tableName);
+    for (const value of values) {
+      const result = await store.put(value);
+      console.log('Put Bulk Data ', JSON.stringify(result));
+    }
+    return this.getAllValue(tableName);
+  }
+
+  async deleteValue(tableName: string, id: number) {
+    const tx = this.db.transaction(tableName, 'readwrite');
+    const store = tx.objectStore(tableName);
+    const result = await store.get(id);
+    if (!result) {
+      console.log('Id not found', id);
+      return result;
+    }
+    await store.delete(id);
+    return id;
+  }
+}
 
 export default new Storage();
