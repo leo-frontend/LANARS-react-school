21b795bd774ebbb9359cf27aed85241da2569546
leo-frontend <frontend@lanars.com>
2022-11-16 16:35:30 +0200

    Fix problems with process

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -2,11 +2,21 @@ import './App.scss';
 import React from 'react';
 
 const App = (): JSX.Element => {
-  const title = 'LANARS react school';
+  // const title = 'LANARS react school';
 
   return (
     <div className="App">
-      {title}
+      <input type="file" id="image-input" accept="image/jpeg, image/png, image/jpg" onChange={(event) => {
+        const reader = new FileReader();
+        // eslint-disable-next-line no-console
+        console.log(event.target.files);
+
+        reader.addEventListener('load', () => {
+          // const uploadedImage = reader.result;
+          // document.querySelector('#display-image').style.backgroundImage = `url(${uploadedImage})`;
+        });
+        // reader.readAsDataURL(files[0]);
+      }}/>
     </div>
   );
 };
