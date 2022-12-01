b54ad74ddfe6781d60de7892656302e1deb11a85
Valentin Hetalo <wallpaperlowup@gmail.com>
2022-12-01 19:54:58 +0200

    Revert "Create interface for Photo and Album"
    This reverts commit 256153da4850c62187cda2d59920081373e443c2.

--- src/shared/interfaces/Album.interface.ts
+++
@@ -1,9 +0,0 @@
-import {PhotoInterface} from './Photo.interface'
-
-export interface AlbumInterface {
-  title: string;
-  description: string;
-  photos: PhotoInterface[];
-  date: number;
-  id?: number;
-}
--- src/shared/interfaces/Photo.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface PhotoInterface {
-  image: string;
-  description: string;
-  date: number;
-  size: number;
-  type: string;
-  id?: number;
-}
