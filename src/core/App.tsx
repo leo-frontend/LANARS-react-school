d8327e6395905ff0358fe3286d10d5bdf5946d03
Taras Honcharuk <taras.goncharuk22@gmail.com>
2022-12-12 22:09:08 +0200

    Add a typed dispatch and useSelector to add to DevTools and visualize the data received

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+
+import {getPhoto} from '../shared/store/photoSlice';
+import {getAlbum} from '../shared/store/albumSlice';
+import {useAppDispatch, useAppSelector} from '../shared/hooks/redux_hooks';
 
 const App = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+  const photo = useAppSelector(state => state.photo.photos);
+  const album = useAppSelector(state => state.album.album);
+
+  useEffect(() => {
+    dispatch(getPhoto([]));
+    dispatch(getAlbum([]));
+  }, []);
+
   return (
     <div className="App">
-
+      {photo.map(item => <img key={item.image} src={item.image} alt={item.type}/>)}
+      {album.map(item => (
+        <div key={item.title}>
+          {item.title}
+          <div>{item.photos}</div>
+        </div>
+      ))}
     </div>
   );
 };
