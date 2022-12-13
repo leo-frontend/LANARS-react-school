18d1fee2757143dd809b7d36bdae2bb5e8ee7203
Taras Honcharuk <taras.goncharuk22@gmail.com>
2022-12-13 20:47:43 +0200

    Change the initial state interface

--- src/shared/interfaces/StateSlices.ts
+++ src/shared/interfaces/StateSlices.ts
@@ -3,7 +3,11 @@ import {IAlbum} from './Album';
 
 export interface IPhotoState {
   photos: IPhoto[];
+  loading: 'idle' | 'pending' | 'succeeded' | 'failed';
+  error: string;
 }
 export interface IAlbumState {
   album: IAlbum[];
+  loading: 'idle' | 'pending' | 'succeeded' | 'failed';
+  error: string;
 }
