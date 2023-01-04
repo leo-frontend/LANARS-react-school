dccb5f059fcfc0646e5068c36cf355db01543809
Karyna-Ko <karina.korshak@gmail.com>
2023-01-04 12:50:34 +0200

    Create App root page

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import { ThemeProvider } from '@emotion/react';
+import { useEffect } from 'react';
+import { useAppDispatch } from 'shared/hooks';
+import { getAlbum } from 'shared/store/reducers/albumReducerSlice';
+import { getPhoto } from 'shared/store/reducers/photoReducerSlice';
+import { theme } from 'styles/theme';
 
 const App = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getPhoto([]));
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(getAlbum([]));
+  }, [dispatch]);
+
   return (
-    <div className="App"></div>
+    <ThemeProvider theme={theme}></ThemeProvider>
   );
 };
 
