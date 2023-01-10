e16f45aa8efb0e029f6738c41d31a7975d05a860
THN91 <taras.gonchahruk22@gmail.com>
2023-01-10 22:24:40 +0200

    Fix deployment error

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -2,11 +2,10 @@ import React, {useEffect} from 'react';
 
 import {getPhoto} from '../shared/store/photoSlice';
 import {getAlbum} from '../shared/store/albumSlice';
-import {useAppDispatch, useAppSelector} from '../shared/hooks/redux_hooks';
+import {useAppDispatch} from '../shared/hooks/redux_hooks';
 
 const App = (): JSX.Element => {
   const dispatch = useAppDispatch();
-  const {photo, album} = useAppSelector(state => state);
 
   useEffect(() => {
     dispatch(getPhoto([]));
