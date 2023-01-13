8206468bfecb5559ea3785c7472330fe28b48349
THN91 <taras.gonchahruk22@gmail.com>
2023-01-13 19:49:19 +0200

    Make component for wrong url

---
+++ src/modules/NotFoundPage.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import {Stack, Typography} from '@mui/material';
+import ReportOutlinedIcon from '@mui/icons-material/ReportOutlined';
+
+
+const NotFoundPage = (): JSX.Element => {
+
+  return (
+    <Stack spacing={2} sx={{height: '50vw', alignItems: 'center', justifyContent: 'center'}}>
+      <ReportOutlinedIcon sx={{color: '#E5EDF2',width: 200,height: 200}}/>
+      <Typography
+        variant={'h1'}
+        component={'div'}>
+        ERROR
+      </Typography>
+      <Typography
+        variant={'body1'}
+        component={'div'}>
+        Page not found
+      </Typography>
+    </Stack>
+  );
+};
+
+export default NotFoundPage;
