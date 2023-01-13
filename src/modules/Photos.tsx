129172eba2ea2874f9af6ba2e7231e835d9b13a1
THN91 <taras.gonchahruk22@gmail.com>
2023-01-13 19:45:50 +0200

    Make a photos component to display photos

---
+++ src/modules/Photos.tsx
@@ -0,0 +1,39 @@
+import React, {useEffect} from 'react';
+
+import {Container, ImageList, ImageListItem} from '@mui/material';
+
+import {getPhoto} from '../shared/store/photoSlice';
+import {useAppDispatch, useAppSelector} from '../shared/hooks/redux_hooks';
+import NotFound from '../shared/components/NotFound';
+
+
+const Photos = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+  const {photo} = useAppSelector(state => state);
+
+  useEffect(() => {
+    dispatch(getPhoto([]));
+  }, []);
+
+
+  return (
+    <Container sx={{mt: 3}}>
+      {photo.photos.length === 0 ?
+        <NotFound name={'photo'} svgSwitch={true}/> :
+        <>
+          <ImageList sx={{mr: 5}} cols={6} gap={8} rowHeight={150}>
+            {photo.photos.map((item) => (
+              <ImageListItem key={item.id}>
+                <img
+                  style={{borderRadius: '10px'}}
+                  src={`data:${item.type};base64,${item.image}`}
+                  alt={item.description}/>
+              </ImageListItem>
+            ))}
+          </ImageList>
+        </>
+      }
+    </Container>
+  );
+};
+export default Photos;
---
+++ src/shared/components/NotFound.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import {Container, styled, Typography} from '@mui/material';
+import InsertPhotoOutlinedIcon from '@mui/icons-material/InsertPhotoOutlined';
+import PhotoAlbumOutlinedIcon from '@mui/icons-material/PhotoAlbumOutlined';
+
+import {MyLink} from '../../styles/styles.';
+
+
+const MainBody = styled(Container)(() => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: '30vw',
+}));
+
+const NotFound = ({name, svgSwitch}: { name: string; svgSwitch: boolean }): JSX.Element => {
+
+  return (
+    <MainBody>
+      {svgSwitch
+        ? <InsertPhotoOutlinedIcon sx={{color: '#E5EDF2', width: 200, height: 200}}/>
+        : <PhotoAlbumOutlinedIcon sx={{color: '#E5EDF2', width: 200, height: 200}}/>
+      }
+      <Typography
+        variant={'subtitle1'}
+        component={'div'}
+        sx={{color: '#6C7276', textAlign: 'center', m: 2, width: 235}}
+      >
+        There are no {name}s yet. Please click
+        <MyLink to={'#'} sx={{fontWeight: 600, color: '#6C7276'}}> Upload {name}
+        </MyLink> to add
+      </Typography>
+    </MainBody>
+  );
+};
+
+export default NotFound;
