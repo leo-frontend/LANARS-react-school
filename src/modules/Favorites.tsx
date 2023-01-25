d093e16cea954ab672aa974d8761fdfd0808330c
THN91 <taras.gonchahruk22@gmail.com>
2023-01-26 01:45:38 +0200

    Write logic favorite

--- src/modules/Favorites.tsx
+++ src/modules/Favorites.tsx
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import {Box, ImageListItem} from '@mui/material';
+import StarIcon from '@mui/icons-material/Star';
 
-import {useAppSelector} from "../shared/hooks/redux_hooks";
+import {useAppDispatch, useAppSelector} from "../shared/hooks/redux_hooks";
+import {getPhoto} from '../shared/store/photoSlice';
+import {colors} from '../styles/variables';
+
 
 
 const Favorites = (): JSX.Element => {
-  const {photo} = useAppSelector(state => state);
+  const dispatch = useAppDispatch();
+  const {photos} = useAppSelector(state => state.photo);
+
+  useEffect(() => {
+    dispatch(getPhoto([]));
+  }, []);
+
+  // const isFavorite = (photoId: number) => photos.find(item=>item.id === photoId && item.isFavorite);
 
   return (
     <Box sx={{display: 'flex', flexWrap: 'wrap', gap: 1, mt: 3}}>
-      {photo.photos.map((item) => item.isFavorite && (
-        <ImageListItem key={item.id}>
-          <img
-            style={{borderRadius: 8, width: 142, height: 142}}
-            src={`data:${item.type};base64,${item.image}`}
-            alt={item.description}/>
-        </ImageListItem>
-      ))}
+      {photos.map((item) => item.isFavorite && (
+          <ImageListItem sx={{position: 'relative'}} key={item.id}>
+            <img
+              style={{borderRadius: 8, width: 142, height: 142}}
+              src={`data:${item.type};base64,${item.image}`}
+              alt={item.description}/>
+            <StarIcon sx={{
+              display: 'flex',
+              position: 'absolute',
+              bottom: '5px',
+              left: '5px',
+              color: colors.light.checkbox,
+            }}/>
+          </ImageListItem>
+        ))}
     </Box>
   );
 };
--- src/modules/Photos.tsx
+++ src/modules/Photos.tsx
@@ -1,35 +1,87 @@
 import React, {useEffect} from 'react';
 
-import {Box, ImageListItem, Stack} from '@mui/material';
+import {Box, Checkbox, ImageListItem, Stack, styled} from '@mui/material';
+import StarIcon from '@mui/icons-material/Star';
 
-import {getPhoto} from '../shared/store/photoSlice';
+import {changeHeader, getPhoto, setChecked} from '../shared/store/photoSlice';
 import {useAppDispatch, useAppSelector} from '../shared/hooks/redux_hooks';
 import NotFound from '../shared/components/NotFound';
 import UploadButton from 'shared/components/UploadButton/UploadButton';
+import {colors} from '../styles/variables';
+
+const MyImageListItem = styled(ImageListItem)(({selected}: { selected: boolean }) => ({
+  borderRadius: 8,
+  position: 'relative',
+  cursor: 'pointer',
+  backgroundColor: colors.light.colorIcon,
+  ['&:hover']: {
+    ['& .MuiCheckbox-root']: {
+      display: 'block',
+    },
+  },
+  ['img']: {
+    transform: selected && 'scale(0.8)',
+  },
+}));
 
 
 const Photos = (): JSX.Element => {
   const dispatch = useAppDispatch();
-  const {photo} = useAppSelector(state => state);
+  const {photos, checked} = useAppSelector(state => state.photo);
+  const arrayCheckedPhoto = Object.entries(checked)
+    .map(item => item[1] && Number(item[0]))
+    .filter(item => item) as number[];
+
 
   useEffect(() => {
     dispatch(getPhoto([]));
   }, []);
 
+  useEffect(() => {
+    dispatch(changeHeader([...arrayCheckedPhoto]));
+  }, [checked]);
+
+  const handlerClick = (photoId: number) =>
+    dispatch(setChecked({...checked, [photoId]: !checked[photoId]}));
+
+  const isFavorite = (photoId: number) => {
+    return photos.find(item=>item.id === photoId+1 && item.isFavorite);
+  }
+
 
   return (
     <>
-      {photo.photos.length === 0 ?
+      {photos.length === 0 ?
         <NotFound name="photo" svgSwitch={true}/> :
         <Stack>
           <Box sx={{display: 'flex', flexWrap: 'wrap', gap: 1, mt: 3}}>
-            {photo.photos.map((item) => (
-              <ImageListItem key={item.id}>
+            {photos.map((item) => (
+              <MyImageListItem
+                key={item.id}
+                selected={!!checked[Number(item.id)]}
+              >
                 <img
                   style={{borderRadius: 8, width: 142, height: 142}}
                   src={`data:${item.type};base64,${item.image}`}
-                  alt={item.description}/>
-              </ImageListItem>
+                  alt="photo"/>
+                <Checkbox
+                  onClick={() => handlerClick(Number(item.id))}
+                  checked={!!checked[Number(item.id)]}
+                  sx={{
+                    display: arrayCheckedPhoto.find(photoId => photoId === item.id) ? 'flex' : 'none',
+                    position: 'absolute',
+                    top: '5px',
+                    right: '5px',
+                    color: colors.light.checkbox,
+                  }}/>
+                <StarIcon sx={{
+                  display: isFavorite(Number(item.id)) ? 'flex' : 'none',
+                  position: 'absolute',
+                  bottom: '5px',
+                  left: '5px',
+                  color: colors.light.checkbox,
+                }}/>
+              </MyImageListItem>
             ))}
           </Box>
           <Box sx={{display: 'flex', justifyContent: 'flex-end', pr: 5}}>
