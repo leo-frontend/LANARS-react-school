6d2be20542c313080ad8e9d5bf7f2cd2de2e0856
THN91 <taras.gonchahruk22@gmail.com>
2023-01-25 17:46:26 +0200

    Add new route

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -3,14 +3,16 @@ import {Route, Routes} from 'react-router';
 
 import {ThemeProvider} from '@mui/material';
 
+import {theme} from '../styles/theme';
 import {AllPath} from '../shared/constants/path';
 import Photos from '../modules/Photos';
 import Albums from '../modules/Albums';
+import Favorites from '../modules/Favorites';
 import Layout from '../shared/components/Layout';
 import NotFoundPage from '../modules/NotFoundPage';
 import Album from '../modules/Album';
 import CreateAlbum from '../modules/CreateAlbum';
-import {theme} from '../styles/theme';
+
 
 
 const App = (): JSX.Element => {
@@ -20,6 +22,7 @@ const App = (): JSX.Element => {
         <Route path={AllPath.ALL_PHOTO} element={<Layout/>}>
           <Route index element={<Photos/>}/>
           <Route path={AllPath.ALBUM} element={<Albums/>}/>
+          <Route path={AllPath.FAVORITES} element={<Favorites/>}/>
           <Route path={AllPath.NOT_FOUND_PAGE} element={<NotFoundPage/>}/>
         </Route>
         <Route path={'albums/:albumId'} element={<Album/>}/>
